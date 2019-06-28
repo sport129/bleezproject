0e1eedea15729ac280751c3b577080d4275252cb
daniel <daniel.rosendos@hotmail.com>
2019-06-27 21:18:52 -0300

    UP CONFIG

---
+++ front/src/config/index.js
@@ -0,0 +1,19 @@
+﻿import axios from 'axios'
+
+const config = {
+  api: {
+    url: 'http://127.0.0.1:8080/api',
+    key: '-',
+    secret: '-',
+    token: btoa(`-:`)
+  },
+  app: {
+    url: 'http://127.0.0.1:8081',
+    storageName: '@BLEEZPROJECT'
+  },
+  pusher: {
+    key: '5e69969fec2396356747'
+  }
+}
+
+export { axios, config }
